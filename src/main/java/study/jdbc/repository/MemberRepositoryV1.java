de1ddf9f1ace41aa372aca363513fc029751680c
thj0309 <thj0309@cj.net>
2022-07-12 13:23:29 +0900

    datasource 최종 버전

--- src/main/java/study/jdbc/repository/MemberRepositoryV1.java
+++ src/main/java/study/jdbc/repository/MemberRepositoryV1.java
@@ -1,17 +1,26 @@
 package study.jdbc.repository;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.support.JdbcUtils;
 import study.jdbc.connection.DBConnectionUtil;
 import study.jdbc.domain.Member;
 
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.NoSuchElementException;
 
 /**
- * JDBC - Jdbc사용
+ * JDBC - DataSource, JdbcUtils 사용
  */
 @Slf4j
-public class MemberRepositoryV0 {
+public class MemberRepositoryV1 {
+
+    private final DataSource dataSource;
+
+    public MemberRepositoryV1(DataSource dataSource){
+        this.dataSource= dataSource;
+    }
+
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values(?, ?)";
         Connection con = null;
@@ -28,28 +37,6 @@ public class MemberRepositoryV0 {
             throw e;
         } finally {
             close(con, pstmt, null);
-        }
-    }
-    private void close(Connection con, Statement stmt, ResultSet rs) {
-        if (rs != null) {
-            try {
-                rs.close();
-            } catch (SQLException e) {
-                log.info("error", e);
-            } }
-        if (stmt != null) {
-            try {
-                stmt.close();
-            } catch (SQLException e) {
-                log.info("error", e);
-            }
-        }
-        if (con != null) {
-            try {
-                con.close();
-            } catch (SQLException e) {
-                log.info("error", e);
-            }
         }
     }
 
@@ -115,8 +102,15 @@ public class MemberRepositoryV0 {
         }
     }
 
-    private Connection getConnection() {
-        return DBConnectionUtil.getConnection();
+    private Connection getConnection() throws SQLException {
+        Connection con = dataSource.getConnection();
+        log.info("Get connetions={}, class={}", con, con.getClass());
+        return con;
     }
 
+    private void close(Connection con, Statement stmt, ResultSet rs) {
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(stmt);
+        JdbcUtils.closeConnection(con);
+    }
 }
